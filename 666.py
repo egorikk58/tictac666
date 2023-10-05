9ccc81718f312aa086359adcd5261695a60cb65a
Egor Andreev <egorka.andreev765@gmail.com>
2023-10-05 13:02:13 +0300

    Ячеейки

--- 666.py
+++ 666.py
@@ -1,1 +1,8 @@
-666
+board = list(range(1, 10))
+
+def f():
+    for i in range(3):
+        print('|', board[0 + i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3],'|')
+print('Выбор ячейки')
+a = input()
+print('Выбрали ',a ' ячейку')
